93f85f5977ac6a4ab3a04357406ddadadff54c20
Tzero Ocne <tzeroocne.interface@gmail.com>
2026-03-17 11:18:26 +0700

    feat(lint): add base linter rule

---
+++ src/index.d.ts
@@ -0,0 +1,17 @@
+import type { Linter } from 'eslint';
+
+type Flat = Linter.FlatConfig;
+
+export declare const base: Flat[];
+export declare const browser: Flat[];
+
+export declare const configs: {
+  base: Flat[];
+  browser: Flat[];
+};
+
+export declare const nnrylint: {
+  configs: typeof configs;
+};
+
+export default nnrylint;
---
+++ src/index.js
@@ -0,0 +1,100 @@
+// @ts-check
+import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import svelteParser from 'svelte-eslint-parser';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+/**
+ * @typedef {import('eslint/rules/stylistic-issues').StylisticIssues} StylisticIssues
+ * @typedef {import('eslint-plugin-svelte/lib/rule-types').RuleOptions} SvelteRulesOptions
+ * @typedef {{ [K in keyof StylisticIssues as `@stylistic/${K}`]: StylisticIssues[K] }} StylisticRules
+ * @typedef {import('eslint/rules/possible-errors').PossibleErrors} PossibleErrors
+ * @typedef {import('eslint/rules/best-practices').BestPractices} BestPractices
+ * @typedef {import('eslint/rules').ESLintRules} ESLintRules
+ * @typedef {PossibleErrors & BestPractices} JSRules
+ * @typedef {{ [K in keyof ESLintRules as `@typescript-eslint/${K}`]: ESLintRules[K] }} TSRules
+ * */
+
+/** @type{Partial<StylisticRules>} */
+const stylisticRules = {
+  '@stylistic/semi': ['error', 'always', { omitLastInOneLineBlock: true }],
+  '@stylistic/comma-dangle': ['error', 'always-multiline'],
+  '@stylistic/quotes': ['error', 'single'],
+  '@stylistic/no-multiple-empty-lines': ['error'],
+  '@stylistic/indent': ['error', 2],
+};
+/** @type {Partial<JSRules>} */
+const jsRules = {
+  'no-empty': ['error'],
+  'no-empty-function': ['error'],
+};
+
+/** @type {Partial<TSRules>} */
+const tsRules = {
+  '@typescript-eslint/no-unused-vars': [
+    'error',
+    {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+      caughtErrorsIgnorePattern: '^_',
+      ignoreRestSiblings: true,
+    },
+  ],
+};
+
+/** @type {Partial<SvelteRulesOptions>} */
+const svelteRules = {
+  'svelte/indent': ['error', {
+    indent: 2,
+  }],
+};
+
+/** @type {import('eslint').Linter.Config[]} */
+export const base = defineConfig([
+  {
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...jsRules,
+      ...tsRules,
+    },
+  },
+  ...tseslint.configs.recommended,
+  ...eslintPluginSvelte.configs['flat/base'],
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: stylisticRules,
+  },
+  {
+    files: ['**/*.svelte', '*.svelte'],
+    rules: svelteRules,
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsparser,
+      },
+    },
+  },
+]);
+
+export const browser = defineConfig([
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  ...base,
+]);
+
+export const configs = {
+  base,
+  browser,
+};
+
+export const nnrylint = {
+  configs,
+};
+
+export default nnrylint;
